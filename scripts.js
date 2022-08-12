1959f4bda1e4ab1589b411097bc1972030e4d53f
Andres C <andrescn20@gmail.com>
2022-08-11 21:12:46 -0600

    PVP logic complete

--- scripts.js
+++ scripts.js
@@ -12,28 +12,83 @@ const gameboard = (() => {
   btns.forEach((btn) => {
     btn.onclick = function (event) {
       btn.innerHTML = playerModule.player1.turn === true ? "X" : "O";
-      let position = btn.id.split("");
-      if (turns % 2 === 0) {
-        playerModule.player2.rows.push(position[0]);
-        playerModule.player2.columns.push(position[1]);
-        console.log(`Player 2 Rows: ${playerModule.player2.rows}`);
-        console.log(`Player 2 Columns: ${playerModule.player2.columns}`);
-      } else {
-        playerModule.player1.rows.push(position[0]);
-        playerModule.player1.columns.push(position[1]);
-        console.log(`Player 1 Rows: ${playerModule.player1.rows}`);
-        console.log(`Player 1 Columns: ${playerModule.player1.columns}`);
-      }
+      currentPlayer =
+        playerModule.player1.turn === true
+          ? playerModule.player1
+          : playerModule.player2;
 
       btn.disabled = true;
       playerModule.player1.turn = !playerModule.player1.turn;
       turns++;
+      mapPositions();
+      comparePosition();
     };
   });
+  const winConditions = [
+    ["A1", "A2", "A3"],
+    ["B1", "B2", "B3"],
+    ["C1", "C2", "C3"],
+    ["A1", "B1", "C1"],
+    ["A2", "B2", "C2"],
+    ["A3", "B3", "C3"],
+    ["A1", "B2", "C3"],
+    ["A3", "B2", "C1"],
+  ];
 
+  const winnerParagraph = document.getElementById("winner");
+  winnerParagraph.innerHTML = "";
+
+  const mapPositions = () => {
+    currentPlayer.position = [];
+    btns.forEach((btn) => {
+      if (btn.innerHTML === currentPlayer.token) {
+        currentPlayer.position.push(btn.id);
+      }
+    });
+  };
+
+  const comparePosition = () => {
+    //Clears property to avoid pushing duplicates.
+    currentPlayer.finalPosition = [];
+
+    //Loops Over Every Combination
+    winConditions.forEach((combination) => {
+      //For Every Combination, loops over every posible field.
+      combination.forEach((field) => {
+        //Checks if the current marked positions have a coincidence and pushes it.
+        if (
+          currentPlayer.position.includes(field) === true &&
+          currentPlayer.finalPosition.includes(field) === false
+        ) {
+          currentPlayer.finalPosition.push(field);
+        }
+      });
+      checkWinner();
+      currentPlayer.finalPosition = [];
+    });
+  };
+  const checkWinner = () => {
+    winConditions.forEach((combination) => {
+      if (currentPlayer.finalPosition.toString() === combination.toString()) {
+        winnerParagraph.innerHTML = `${currentPlayer.name} is the winner`;
+        btns.forEach((btn) => {
+          btn.disabled = true;
+        });
+      }
+      // } else if (turns === 10) {
+      //   winnerParagraph.innerHTML = "It's a draw";
+      // }
+    });
+  };
   //Restart Button
   const restartBtn = document.getElementById("restart");
   restartBtn.onclick = function (event) {
+    //Reset Turn counter and status
+    turns = 1;
+    playerModule.player1.turn = true;
+    playerModule.player2.turn = false;
+    winnerParagraph.innerHTML = "";
+
     //Clean Board
     btns.forEach((btn) => {
       btn.innerHTML = "";
@@ -52,10 +107,10 @@ const playerModule = (() => {
   let btns = Array.from(document.getElementsByClassName("choosePlayer"));
 
   //Player Factory Function
-  const playerFactory = (_player, token, turn, status) => {
-    const rows = [];
-    const columns = [];
-    return { token, turn, rows, columns };
+  const playerFactory = (player, token, turn) => {
+    const position = [];
+    const finalPosition = ["hello"];
+    return { token, turn, position, name: player, finalPosition };
   };
 
   const player1 = playerFactory("player1", "X", true, true);
